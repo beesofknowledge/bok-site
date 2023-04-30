26f06ee85c9544ba18b9c581350cbac6355b4c71
belarios <belarios@rocketmail.com>
2023-04-30 18:16:52 -0400

    add sorting to quest list

--- docs/.vitepress/data/questlist.data.js
+++ docs/.vitepress/data/questlist.data.js
@@ -6,6 +6,22 @@ const file = 'docs/.vitepress/data/quests.yaml'
 export default {
   watch: [file],
   load(watchedFiles) {
-    return parse(fs.readFileSync(file, 'utf-8'))
+    const data = parse(fs.readFileSync(file, 'utf-8'))
+
+    data.sort(function(a, b) {
+      const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA > nameB) {
+        return 1;
+      }
+      if (nameA < nameB) {
+        return -1;
+      }
+
+      // names must be equal
+      return 0;
+    });
+
+    return data
   }
 }
