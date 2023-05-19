8c153f4ed5f199ac8099c282ad21d836f30b5f90
belarios <belarios@rocketmail.com>
2023-05-19 11:10:11 -0400

    move items yml to csv and add items

--- docs/.vitepress/data/itemlist.data.js
+++ docs/.vitepress/data/itemlist.data.js
@@ -1,13 +1,18 @@
 import fs from 'node:fs'
-import { parse } from 'yaml'
 import slugify from 'slugify'
+//import { parse } from 'yaml'
+import { parse } from 'csv-parse/sync'
 
-const file = 'docs/.vitepress/data/items.yml'
+//const file = 'docs/.vitepress/data/items.yml'
+const file = 'docs/.vitepress/data/items.csv'
 
 export default {
   watch: [file],
   load(watchedFiles) {
-    let data = parse(fs.readFileSync(file, 'utf-8'))
+    let data = parse(fs.readFileSync(file, 'utf-8'), {
+      columns: true,
+      skip_empty_lines: true,
+    })
 
     data.sort(function(a, b) {
       const nameA = a.name.toUpperCase()
--- docs/items/[item].paths.js
+++ docs/items/[item].paths.js
@@ -1,12 +1,17 @@
 import fs from 'node:fs'
-import { parse } from 'yaml'
 import slugify from 'slugify'
+//import { parse } from 'yaml'
+import { parse } from 'csv-parse/sync'
 
-const file = 'docs/.vitepress/data/items.yml'
+//const file = 'docs/.vitepress/data/items.yml'
+const file = 'docs/.vitepress/data/items.csv'
 
 export default {
   paths() {
-    const data = parse(fs.readFileSync(file, 'utf-8'))
+    const data = parse(fs.readFileSync(file, 'utf-8'), {
+      columns: true,
+      skip_empty_lines: true,
+    })
 
     return data.map((datum) => {
       const slug = slugify(datum.name, {
